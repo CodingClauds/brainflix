e8acbd95c924524f4d861a1c62783597c3611422
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-05-26 23:52:25 -0400

    Updated the markup and styles, got the mobile mockup to resemble

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.scss";
-import "../../styles/partials/_globals.scss";
 import siteLogo from '../../assets/Logo/BrainFlix-logo.svg';
-import navAvatar from '../../assets/Images/Mohan-muruge.jpg';
+import userLogo from '../../assets/Images/Mohan-muruge.jpg';
+import upload from '../../assets/Icons/upload.svg'
 import searchIcon from '../../assets/Icons/search.svg';
 
 
@@ -9,26 +9,29 @@ function Header() {
     return (
         <header className="header">
           <nav className="header__nav">
-            
             <div className="header__logo-box">
               <img className="header__img" src={siteLogo} alt="brainflix logo" />
             </div>
-          
-            <div className="header__form-box">
-              <form className="header__form" action="">
-                <input className="header__search" src={searchIcon} type="text" placeholder='Search'/>
-                {/* background the search img in styles */}
-              
-                <img className='header__avatar-img' src={navAvatar} alt="nav avatar with someones profile photo"/>
-              </form>
 
-              <button className="header__btn">UPLOAD</button>
+            <div className="header__search-box">
+                {/* background the search img in styles */}
+              <form className="header__form">
+                <input className="header__search" src={searchIcon} type="text" placeholder='Search'/>
+              </form>
+                
+              <div className="header__avatar-box">
+                <img className='header__user-logo' src={userLogo} alt="user profile with someones photo"/>
+              </div>
             </div>
-  
+
+            <button className="header__btn">
+              <img className="header__btn-icon" src={upload} alt="upload button"/>
+                <p className="header__btn-text">Upload</p>
+              <div className="header__btnbox"></div>
+            </button>
           </nav>
         </header>
     );
   }
 
 export default Header;
-  
