ff233a84e78c2187431593748ce00027bb0be205
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-05-28 23:20:12 -0400

    Mobile styling 80% of the way

--- src/components/VideoDescription/VideoDescription.js
+++ src/components/VideoDescription/VideoDescription.js
@@ -5,36 +5,36 @@ import likes from '../../assets/Icons/likes.svg';
 export default function VideoDescription({ selectedVideo }) {
   return (
     <>
-        <section className = 'vid-info__section'>
-            <div className = 'vid-info__content' key={selectedVideo.id}>
-                <h1 className = "vid-info__title">{selectedVideo.title}</h1>
+        <section className = 'video-info__section'>
+            <div className = 'video-info__content' key={selectedVideo.id}>
+                <h1 className = "video-info__title">{selectedVideo.title}</h1>
                 
-                <ul className = "vid-info__list">
-                    <li className = 'vid-info__item artist'>{selectedVideo.channel}</li>
+                <ul className = "video-info__list">
+                    <li className = 'video-info__item artist'>{selectedVideo.channel}</li>
                     
-                    <li className = 'vid-info__item'>
-                        <div className = "vid-info__container">
-                            <img className = "vid-info__icon" src={views} alt="views icon"></img>
+                    <li className = 'video-info__item'>
+                        <div className = "video-info__container">
+                            <img className = "video-info__icon" src={views} alt="views icon"></img>
                         </div>
                         
-                        <span className = "vid-info__item views">{selectedVideo.views}</span>
+                        <span className = "video-info__item views">{selectedVideo.views}</span>
                     </li>
                     
-                    <li className = 'vid-info__item timestamp'>{new Date(selectedVideo.timestamp).toLocaleDateString()}</li>
+                    <li className = 'video-info__item timestamp'>{new Date(selectedVideo.timestamp).toLocaleDateString()}</li>
 
-                    <li className = 'vid-info__item'>
-                        <div className = "vid-info__container">
-                            <img className = "vid-info__icon" src={likes} alt="likes icon"></img>
+                    <li className = 'video-info__item'>
+                        <div className = "video-info__container">
+                            <img className = "video-info__icon" src={likes} alt="likes icon"></img>
                         </div>
 
-                        <span className = "vid-info__likes">{selectedVideo.likes}</span>
+                        <span className = "video-info__likes">{selectedVideo.likes}</span>
                     </li>
                 </ul>
             </div>
             
-            <div className = "vid-info__content">
-                <p className = "vid-info__description">{selectedVideo.description}</p>
-                <span className = "vid-info__comment-count">3 comments</span>
+            <div className = "video-info__content">
+                <p className = "video-info__description">{selectedVideo.description}</p>
+                <span className = "video-info__comment-count">3 comments</span>
             </div>
         </section>
     </>
